fee360631eabd5f98579a74382167f41c7bcd7a4
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-11-10 13:54:41 +0300

    installPackages

--- week03/src/main.js
+++ week03/src/main.js
@@ -3,10 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+/* eslint-disable */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import vuetify from './plugins/vuetify'
+
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount("#app");
