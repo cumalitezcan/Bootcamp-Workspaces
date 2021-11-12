6baaa548b51b50382d31352273718dfcc3d25159
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-11-12 11:47:26 +0300

    getDailyValues

--- week03/src/store/index.js
+++ week03/src/store/index.js
@@ -7,7 +7,10 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     search: null,
-    companyAndSymbolValues: []
+    companyAndSymbolValues: [],
+    dailyValues: [],
+    weeklyValues: [],
+    monthlyValues: [],
     
   },
   mutations: {
@@ -17,6 +20,16 @@ export default new Vuex.Store({
     SET_COMPANY_SEARCH_RESULT(state, payload) {
       state.companyAndSymbolValues = payload;
     },
+    SET_DAILY_RESULTS(state, payload) {
+      state.dailyValues = payload;
+    },
+    SET_WEEKLY_RESULTS(state, payload) {
+      state.weeklyValues = payload;
+    },
+    SET_MONTLY_RESULTS(state, payload) {
+      state.monthlyValues = payload;
+    },
+
   },
   actions: {
     searchFromValue({ state, commit }) {
@@ -38,15 +51,42 @@ export default new Vuex.Store({
         .request(options)
         .then((res) => {
           let obj = res.data.bestMatches
-          console.log("geldi1")
-           console.log(obj)
           commit("SET_COMPANY_SEARCH_RESULT",obj);
         })
         .catch((err) => {
-          console.log("gelmedi2")
           console.log(err);
         });
-    },
+    
+  },
+
+  dailyValues({ state, commit },payload){
+    
+    const options = {
+      method: 'GET',
+      url: 'https://alpha-vantage.p.rapidapi.com/query',
+      params: {
+        function: 'TIME_SERIES_DAILY',
+        symbol: payload,
+        outputsize: 'compact',
+        datatype: 'json'
+      },
+      headers: {
+        'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+        'x-rapidapi-key': '83b6a8d056msh586464f5c969161p114268jsnd0bb370711c6'
+      },
+    };
+    axios
+    .request(options)
+    .then((res) => {
+      console.log(res.data["Time Series (Daily)"])
+    let obj = res.data["Time Series (Daily)"]
+    commit("SET_DAILY_RESULTS",obj);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+},
+
   },
   getters:{
     companyNameAndSymbol: (state) => {
--- week03/src/views/Search.vue
+++ week03/src/views/Search.vue
@@ -6,8 +6,14 @@
 </template>
 
 <script>
-
+// import {  mapMutations } from "vuex";
 export default {
   name: "Search",
+
+  created(){
+    console.log(this.$route.params)
+    console.log(this.$route.params.symbol)
+    this.$store.dispatch("dailyValues",this.$route.params.symbol);
+  }
 };
 </script>
